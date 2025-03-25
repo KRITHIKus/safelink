aff13d012dc36086ce9a90ebe789c8241705758d
KRITHIKus <krithikus2004@gmail.com>
2025-03-25 14:12:32 +0530

    cleared build.sh chrome install error on render

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,11 @@ set -eux
 # Check if running on Windows
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     echo "Detected Windows OS. Skipping Linux-specific commands."
+else
+    # Install Chrome using a direct .deb package
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    apt-get update
+    apt-get install -y ./google-chrome-stable_current_amd64.deb
 fi
 
 # Install Python dependencies
